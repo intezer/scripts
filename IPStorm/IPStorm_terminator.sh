0c9264a714da9bc926ea4b6422e08db1b79866cc
Avigayil <avigayil@intezer.com>
2020-10-05 20:08:37 +0300

    add IPStorm terminator

--- IPStorm/IPStorm_terminator.sh
+++ IPStorm/IPStorm_terminator.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 STORM_ROOT_LOCATION=/usr/bin/storm
 STORM_TMP_LOCATION=/tmp/storm
 STORM_SERVICE_NAME=storm.service
 STORM_PROCESS_NAME=storm
 
-if pidof "$STORM_PROCESS_NAME" &> /dev/null; then
+if pidof "$STORM_PROCESS_NAME"; then
   
   echo "[!] IPStorm is running on your system."
   echo "# Cleaning machine from IPStorm..."
   sudo rm -rf "$STORM_ROOT_LOCATION" "$STORM_TMP_LOCATION"
-  sudo systemctl stop "$STORM_SERVICE_NAME" &> /dev/null
+  sudo systemctl stop "$STORM_SERVICE_NAME" 2> /dev/null
   sudo pkill -9 "$STORM_PROCESS_NAME" &> /dev/null
-  
+  sleep 2
   if pidof "$STORM_PROCESS_NAME"; then
 	echo "# Error in killing IPStorm. Please contact Intezer for support"
   else
@@ -22,5 +22,3 @@ if pidof "$STORM_PROCESS_NAME" &> /dev/null; then
  else
 	echo "# IPStorm is not running on your system"
 fi
-
-
